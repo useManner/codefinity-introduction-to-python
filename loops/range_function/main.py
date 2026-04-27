ce60cc94238d0623d7ddb5a3037b3acbebb19245
useManner <>
2026-04-27 08:45:32 +0000

    Range Function

---
+++ loops/range_function/main.py
@@ -0,0 +1,14 @@
+# for day in range(7):
+#     print(day)
+# for day in range(25, 32):
+#     print(day)
+# for hour in range(1,13,3): # 1 : 代表开始值，13：代表停止值，3：代表每次的步数是几
+#     print(hour)
+
+
+# List of products on promotion for each weekday
+daily_promotions = ["Milk", "Eggs", "Bread", "Apples", "Oranges"]
+
+# List of weekdays corresponding to the promotions
+weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
